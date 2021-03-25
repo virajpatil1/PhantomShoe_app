9a5f8191500dbd9fd7b34515caaf1264fd4fe412
VIRAJ <viraj@VIRAJs-MacBook-Pro.local>
2021-03-25 14:05:33 +0530

    Prepare for deployment

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+ 
+    name: 'Nike Air Max 90 Infrared (2020)',
+    image: '/images/airmax.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Nike',
+    category: 'Shoe',
+    price: 4000,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+  
+    name: 'Nike React Element 87 Anthracite Black',
+    image: '/images/elements.jpg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Addidas',
+    category: 'Shoe',
+    price: 5500,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+
+    name: 'Nike LD Waffle Sacai White Nylon',
+    image: '/images/LDwaffle.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Nike',
+    category: 'Shoe',
+    price: 2500,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    
+    name: 'Jordan 1 Retro Low OG SP Travis Scott',
+    image: '/images/travisSB.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Nike',
+    category: 'Shoe',
+    price: 6000,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    
+    name: 'adidas Yeezy QNTM (Lifestyle Model)',
+    image: '/images/yezzy.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Addidas',
+    category: 'Shoe',
+    price: 8000,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+   
+    name: 'adidas Yeezy 500 Stone',
+    image: '/images/yezzy500.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Addidas',
+    category: 'Shoe',
+    price: 6000,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
